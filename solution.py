35c5df59a8ede28bee7c1106a118496b36569cd7
montoyjh <montoyjh@gmail.com>
2018-09-25 20:26:04 -0700

    update main script

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 # Author: J. Montoya - montoyjh@gmail.com
 
-from distributor import Distributor
+from distribution import Distribution
 import sys
 import argparse
 
@@ -9,25 +9,24 @@ desc = 'Program that takes two filename inputs corresponding \
         to lists of files and nodes and distributes the files \
         onto the nodes such that the absolute loads are as \
         close to one another as possible'
-    
+
 arg_parser = argparse.ArgumentParser(description=desc)
-arg_parser.add_argument('-f',metavar='FILES_FILENAME',
-                    help='File containing file list',
-                    required=True)
-arg_parser.add_argument('-n',metavar='NODES_FILENAME',
-                    help='File containing node list',
-                    required=True)
-arg_parser.add_argument('-o',metavar='OUTPUT_FILENAME',
-                    help='output file, optional, default is stdout')
-arg_parser.add_argument('-p',action='store_true',
-                    help='plotting flag, plots nodes/files on bar chart')
+arg_parser.add_argument('-f', '--files', help='File containing file list',
+                        required=True)
+arg_parser.add_argument('-n', '--nodes', help='File containing node list',
+                        required=True)
+arg_parser.add_argument('-o', '--output',
+                        help='output file, optional, default is stdout')
+arg_parser.add_argument('-p', '--plot', action='store_true',
+                        help='plotting flag, plots nodes/files on bar chart')
 args = arg_parser.parse_args()
 
 if args.o:
-    sys.stdout = open(args.o,'w')
+    sys.stdout = open(args.o, 'w')
 # Main body
-dist = Distributor(args.f,args.n)
+dist = Distribution(args.f, args.n)
 dist.distribute()
 dist.summary()
-if args.p:
+
+if args.plot:
     dist.plot()
